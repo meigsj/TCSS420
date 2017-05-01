88af45417812b8af60dfa83de76acf9a7ffb41d9
Shaun Coleman <shaunpcole@gmail.com>
2017-04-30 21:30:32 -0700

    Semi fixed Switch

--- jminusminusTester.java
+++ jminusminusTester.java
@@ -1,54 +1,16 @@
 
 public class jminusminusTester {
 	
-	
-<<<<<<< HEAD
 	void testMethod() {
 		int i  = 0;
 		switch(i) {
-			case 0: { int p = 2;}
-			case 1: {int p = 2;}
-			default: {int p = 2;}
-=======
-	void standardFor() throws IOException, IllegalArgumentException {
-		for(int i = 0; i < 10; i = i + 1) {
-			System.out.println(i);
-		}
-	}
-	
-	
-	void standardForNoInit() throws IOException, IllegalArgumentException {
-		int i = 0;
-		for(; i < 10; i = i + 1) {
-			System.out.println(i);
-		}
-	}
+			case 0:  
+				i = i + 1;
+			case 1: 
+				i = i + 2;
+			default: 
+				i = i + 3;
 
-	void standardForNoCondition() throws IOException, IllegalArgumentException {
-		int i = 0;
-		for(; ;i = i + 1) {
-			System.out.println(i);
-		}
-	}
-	
-	void standardForNoIncrement() throws IOException, IllegalArgumentException {
-		int i = 0;
-		for(; ;) {
-			System.out.println(i);
-		}
-	}
-	
-	void standardForOnlyCond() throws IOException, IllegalArgumentException {
-		int i = 0;
-		for(;i < 10 ;) {
-			System.out.println(i);
-		}
-	}
-	
-	void standardForOnlyInit() throws IOException, IllegalArgumentException {
-		for(int i = 0;;) {
-			System.out.println(i);
->>>>>>> 739932829248be00901cd1cc5808488c268152fa
 		}
 	}
 }
--- src/jminusminus/Parser.java
+++ src/jminusminus/Parser.java
@@ -682,7 +682,8 @@ public class Parser {
             ArrayList<JExpression> caseLiterials = new ArrayList<>();
             ArrayList<ArrayList<JStatement>> caseStatements = new ArrayList<>();
             while(have(CASE)) {
-            	caseLiterials.add(literal());         	
+            	caseLiterials.add(literal());
+            	mustBe(TERNARY_COLON);
             	ArrayList<JStatement> singleCaseStatement = new ArrayList<>();
             	while(!see(CASE) && !see(DEFAULT)&& !see(RCURLY)) {
                 	singleCaseStatement.add(statement());
@@ -690,7 +691,8 @@ public class Parser {
             	caseStatements.add(singleCaseStatement);
             }    
             ArrayList<JStatement> defaultStatements = new ArrayList<>();
-            if(have(DEFAULT)) {  	
+            if(have(DEFAULT)) { 
+            	mustBe(TERNARY_COLON);
             	while(!see(RCURLY)) {
             		defaultStatements.add(statement());
             	}
@@ -736,8 +738,6 @@ public class Parser {
             mustBe(SEMI);
             return statement;
         }
-        // TODO Remove after stub out is filled
-        return null;
     }
     
     private JStandardForStatement getStandardFor(int line, JStatement init) {
