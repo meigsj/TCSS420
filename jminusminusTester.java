ce7a30718d0ea93ba1d71d580e5e0f494b32576e
Shaun Coleman <shaunpcole@gmail.com>
2017-04-30 23:39:59 -0700

    Removed some TODOs

--- jminusminusTester.java
+++ jminusminusTester.java
@@ -3,14 +3,9 @@ public class jminusminusTester {
 	
 	void testMethod() {
 		int i  = 0;
-		switch(i) {
-			case 0:  
-				i = i + 1;
-			case 1: 
-				i = i + 2;
-			default: 
-				i = i + 3;
-
+		while (i < 2) {
+			int j = 1;
+			i = i + j;
 		}
 	}
 }
